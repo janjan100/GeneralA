ffdea6ef16dfbe3f483554d3fd48d0372c00f04f
Janell Forchu <jforchu@gmail.com>
2022-11-15 20:25:37 -0500

    Finalproject1_jf

---
+++ Homework/Final Project2/fashion.js
@@ -0,0 +1,8 @@
+const carousel = new bootstrap.Carousel('#myCarousel');
+// for carousel pp sliding
+
+const myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', event => {
+  // do something...
+})
